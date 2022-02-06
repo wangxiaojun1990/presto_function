7caa1b01b82698110213f932ed526b8a094976c5
chandler <524364896@qq.com>
2022-02-06 21:22:20 +0800

    init

--- src/main/java/com/chandler/demo/BaseFunc.java
+++ src/main/java/com/chandler/demo/BaseFunc.java
@@ -1,18 +1,34 @@
 package com.chandler.demo;
 
 import com.facebook.presto.common.type.StandardTypes;
-import com.facebook.presto.spi.function.Description;
-import com.facebook.presto.spi.function.ScalarFunction;
-import com.facebook.presto.spi.function.SqlNullable;
-import com.facebook.presto.spi.function.SqlType;
+import com.facebook.presto.spi.function.*;
+import com.github.javafaker.Faker;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+
+import java.util.Locale;
+
 
 public class BaseFunc  {
+
+    private static Faker faker = new Faker(Locale.CHINA);
+
     @ScalarFunction(value = "is_null", calledOnNullInput = true)
     @Description("Returns TRUE if the argument is NULL")
     @SqlType(StandardTypes.BOOLEAN)
-    public static boolean isNull(@SqlNullable @SqlType(StandardTypes.VARCHAR) Slice string)
-    {
+    public static boolean isNull(@SqlNullable @SqlType(StandardTypes.VARCHAR) Slice string) {
         return (string == null);
     }
+
+
+    @ScalarFunction(value = "datagen_address", calledOnNullInput = true)
+    @Description("Returns random address string")
+    @TypeParameter("T")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice DatagenAddress(@SqlNullable @SqlType("T") Object value) {
+        return Slices.utf8Slice(faker.address().fullAddress());
+    }
+
+
+
 }
